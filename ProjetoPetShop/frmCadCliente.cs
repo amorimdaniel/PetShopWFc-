02275aae0ae2f3d9755ba39a5678c74600b6b395
Daniel Amorim <dan-amorim@live.com>
2022-12-15 11:12:29 -0300

    Implentado botões salvar, editar e cancelar

--- ProjetoPetShop/frmCadCliente.cs
+++ ProjetoPetShop/frmCadCliente.cs
@@ -16,5 +16,45 @@ namespace ProjetoPetShop
         {
             InitializeComponent();
         }
+
+        private void clienteBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.clienteBindingSource.EndEdit();
+                clienteTableAdapter.Update(petshopDataSet.cliente);
+                groupBox1.Enabled = false;
+                MessageBox.Show("Regristro salvo");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro");
+            }
+        }
+
+        private void frmCadCliente_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'petshopDataSet.cliente' table. You can move, or remove it, as needed.
+            this.clienteTableAdapter.Fill(this.petshopDataSet.cliente);
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            clienteBindingSource.AddNew();
+            groupBox1.Enabled = true;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            clienteBindingSource.CancelEdit();
+            groupBox1.Enabled = false;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+        }
     }
 }
